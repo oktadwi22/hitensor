254d234343efc8564232bee62ba86634638b3c88
linxcode <linvxmusic@gmail.com>
2024-04-07 00:41:07 +0700

    fix:error

--- src/common/component/element/ComponentTransition.tsx
+++ src/common/component/element/ComponentTransition.tsx
@@ -1,6 +1,5 @@
 'use client'
 import React from "react";
-import { translateInViewVariant } from "../../constant/Transition";
 import { motion } from "framer-motion";
 
 interface TransitionProps {
@@ -8,6 +7,19 @@ interface TransitionProps {
   className?: string;
   delay?: number;
   style?: any;
+}
+export const translateInViewVariant = {
+  initial: {
+    opacity: 0,
+    y: 90,
+    // height: '10px'
+  },
+  whileInview: {
+    opacity: 1,
+    y: 0,
+    // height: 'auto',
+
+  },
 }
 
 const ComponentTransition = ({ children,className, delay,style}: TransitionProps) => {
